d61345e1de4d45eda998381fdb3152ec0c5ab8c3
JeffHarris95 <jeff.harris95@yahoo.com>
2021-11-19 20:23:37 -0600

    Pokemon

---
+++ pokemon-api/src/App.js
@@ -0,0 +1,22 @@
+import './App.css';
+import React, {useState, useEffect} from 'react';
+function App() {
+
+  const [pokemon, setPokemon] = useState([]);
+
+  useEffect(() => {
+    fetch('https://pokeapi.co/api/v2/pokemon?limit=10000')
+      .then(response => response.json())
+      .then(response => setPokemon(response.results))
+  },[]);
+
+  return (
+    <div className="App">
+      {pokemon.length > 0 && pokemon.map((pokemon, index)=>{
+        return (<div key={index}>{pokemon.name}</div>)
+      })}
+    </div>
+  );
+}
+
+export default App;
