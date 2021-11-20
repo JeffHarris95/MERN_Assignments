f1376d6c17d9e56c93b2181a6047b0bd4d88de9e
JeffHarris95 <jeff.harris95@yahoo.com>
2021-11-19 20:45:45 -0600

    Pokemon

--- pokemon-api/src/App.js
+++ pokemon-api/src/App.js
@@ -1,13 +1,13 @@
 import './App.css';
 import React, {useState, useEffect} from 'react';
+import axios from 'axios';
 function App() {
 
   const [pokemon, setPokemon] = useState([]);
 
   useEffect(() => {
-    fetch('https://pokeapi.co/api/v2/pokemon?limit=10000')
-      .then(response => response.json())
-      .then(response => setPokemon(response.results))
+    axios.get('https://pokeapi.co/api/v2/pokemon?limit=10000')
+      .then(response => {setPokemon(response.data.results)})
   },[]);
 
   return (
