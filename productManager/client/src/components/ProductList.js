5c0be868d98ff8d28557b1af1bbc788d0afda858
JeffHarris95 <jeff.harris95@yahoo.com>
2021-12-11 22:48:53 -0600

    Product 3

--- productManager/client/src/components/ProductList.js
+++ productManager/client/src/components/ProductList.js
@@ -1,10 +1,25 @@
 import React from 'react'
 import { Link } from '@reach/router'
+import axios from 'axios';
 const ProductList = (props) => {
+    const { removeFromDom, products} = props;
+    const deleteProduct = (productId) => {
+        axios.delete('http://localhost:8000/api/product/' + productId)
+            .then(res => {
+                removeFromDom(productId)
+            })
+            .catch(err => console.log(err))
+    }
     return (
         <div>
-            {props.products.map((product, idx)=>{
-                return  <Link to={"/product/" + product._id}><p key={idx}>{product.title}</p></Link>
+            {products.map((product, idx)=>{
+                return (
+                    <div>
+                        <Link to={"/product/" + product._id}><p key={idx}>{product.title}</p></Link>
+                        <Link to={"/product/" + product._id + "/edit"}>Edit</Link>
+                        <button onClick={(e)=>{deleteProduct(product._id)}}>Delete</button>
+                    </div>
+                )
             })}
         </div>
     )
--- productManager/client/src/views/Detail.js
+++ productManager/client/src/views/Detail.js
@@ -8,11 +8,23 @@ const Detail = (props) => {
                 ...res.data
             }))
     }, [])
+
+    const { removeFromDom, products} = props;
+    const deleteProduct = (productId) => {
+        axios.delete('http://localhost:8000/api/product/' + productId)
+            .then(res => {
+                removeFromDom(productId)
+            })
+            .catch(err => console.log(err))
+
+    }
     return (
         <div>
             <p>Title: {product.title}</p>
             <p>Price: {product.price}</p>
             <p>Description: {product.description}</p>
+
+            <button onClick={(e)=>{deleteProduct(product._id)}}>Delete</button>
         </div>
     )
 }
