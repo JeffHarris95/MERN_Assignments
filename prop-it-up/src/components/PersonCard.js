4572567a7f2759e917fc8f993f62f5d088a2271a
JeffHarris95 <jeff.harris95@yahoo.com>
2021-12-22 21:14:04 -0600

    Props 2

--- prop-it-up/src/components/PersonCard.js
+++ prop-it-up/src/components/PersonCard.js
@@ -1,16 +1,26 @@
-import React, { Component } from 'react';
+import React, { Component, useState } from 'react';
 
 class PersonCard extends Component {
+    constructor(props) {
+        super(props);
+    
+        this.state = {
+            birthday: this.props.age,
+        };
+    }
+
     render() {
-        const { firstName, lastName, age, hairColor } = this.props;
+        const { firstName, lastName, hairColor } = this.props;
+
+
         return (
             <div>
                 <h1>{ lastName }, { firstName }</h1>
-                <p>Age: { age }</p>
+                <p>Age: { this.state.birthday }</p>
                 <p>Hair Color: {hairColor }</p>
+                <button onClick={() => this.setState({ birthday: this.state.birthday + 1 })}>Birthday button for { firstName } { lastName }</button>
             </div>
         );
     }
-}
-
+    }
 export default PersonCard;
